48bbf9341ddc2e8a42764fa0043ba495c1fbd8d9
Drew Hornbein <dhornbein@gmail.com>
2016-07-19 13:49:46 -0400

    configure css

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var marked = require('marked');
 
 module.exports = {
+    website: {
+        assets: "./book",
+        css: [
+        "test.css"
+        ]
+    },
     // Extend templating blocks
     blocks: {
         // Author will be able to write "{% section %}World{% endSection %}"
         section: {
             process: function(blk) {
-                return '<section>'
+                return '<section class="call-out">'
                 + marked(blk.body)
                 + '</section>';
             }
