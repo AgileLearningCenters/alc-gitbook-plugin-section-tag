f8737aad76666e35edf2a318d97612dcf1f1a28a
Drew Hornbein <dhornbein@gmail.com>
2016-07-19 19:20:56 -0400

    added default ALC style

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ module.exports = {
         // Author will be able to write "{% section %}World{% endSection %}"
         section: {
             process: function(blk) {
-                return '<section class="call-out">'
+                var arg1 = (blk.args[0]) ? blk.args[0] : 'callout',
+                    arg2 = (blk.args[1]) ? blk.args[1] : 'default',
+                    argClass = arg1 + ' ' + arg1 + '-' + arg2;
+
+                return '<section class="' + argClass + '">'
                 + marked(blk.body)
                 + '</section>';
             }
