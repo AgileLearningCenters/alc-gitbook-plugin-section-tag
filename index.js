927e9e83a2178a13685906da63b4b15fa15c8c9b
Drew Hornbein <dhornbein@gmail.com>
2016-07-20 10:51:09 -0400

    changed section to aside

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ module.exports = {
     },
     // Extend templating blocks
     blocks: {
-        // Author will be able to write "{% section %}World{% endSection %}"
-        section: {
+        // Author will be able to write "{% aside %}World{% endaside %}"
+        aside: {
             process: function(blk) {
                 var arg1 = (blk.args[0]) ? blk.args[0] : 'callout',
                     arg2 = (blk.args[1]) ? blk.args[1] : 'default',
                     argClass = arg1 + ' ' + arg1 + '-' + arg2;
 
-                return '<section class="' + argClass + '">'
+                return '<aside class="' + argClass + '">'
                 + marked(blk.body)
-                + '</section>';
+                + '</aside>';
             }
         }
     }
