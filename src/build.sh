5888ab33650897481c3daff4b729a3f2c936ae5c
Drew Hornbein <dhornbein@gmail.com>
2016-07-24 13:56:46 -0400

    font handling

--- src/build.sh
+++ src/build.sh
@@ -9,11 +9,19 @@ mkdir -p _assets/ebook/
 
 # Compile SASS
 
-node-sass --output-style compressed src/scss/website.scss _assets/website/alc-style.css
-node-sass --output-style compressed src/scss/ebook.scss _assets/ebook/alc-ebook.css
-node-sass --output-style compressed src/scss/epub.scss _assets/ebook/alc-epub.css
-node-sass --output-style compressed src/scss/mobi.scss _assets/ebook/alc-mobi.css
-node-sass --output-style compressed src/scss/pdf.scss _assets/ebook/alc-pdf.css
+# node-sass --output-style compressed src/scss/website.scss _assets/website/alc-style.css
+# node-sass --output-style compressed src/scss/ebook.scss _assets/ebook/alc-ebook.css
+# node-sass --output-style compressed src/scss/epub.scss _assets/ebook/alc-epub.css
+# node-sass --output-style compressed src/scss/mobi.scss _assets/ebook/alc-mobi.css
+# node-sass --output-style compressed src/scss/pdf.scss _assets/ebook/alc-pdf.css
+
+node-sass src/scss/website.scss _assets/website/alc-style.css
+node-sass src/scss/ebook.scss _assets/ebook/alc-ebook.css
+node-sass src/scss/epub.scss _assets/ebook/alc-epub.css
+node-sass src/scss/mobi.scss _assets/ebook/alc-mobi.css
+node-sass src/scss/pdf.scss _assets/ebook/alc-pdf.css
 
 # Move Fonts
 
+mkdir -p _assets/website/fonts
+cp -R src/fonts/ _assets/website/fonts/
